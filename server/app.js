5bd9e6134c6b2a8732e3636b78d8b8ef01405264
Biraj Ghorasaini <biraj.growcts@gmail.com>
2025-07-19 15:28:40 +0545

    🚀 Backend server is done

--- server/app.js
+++ server/app.js
@@ -1,13 +1,33 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import errorHandler from "./src/middlewares/errorHandler.js";
+
+dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use(errorHandler);
 
 export default app;
