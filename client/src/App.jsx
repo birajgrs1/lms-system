b96de55393f26887691182e382baabe5c54da7f3
Biraj Ghorasaini <biraj.growcts@gmail.com>
2025-07-19 14:38:20 +0545

    started creating final project lms, setting up all necessary installations and setup project completed

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center h-screen text-3xl font-bold'>
+      <h2 className='text-center text-violet-600'>Learning Management System - EduHub</h2>
+    </div>
+  )
+}
+
+export default App
